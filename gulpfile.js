1a8cb75806f051c2eed594cbcded32ae7e3d4c22
Lucas Blancas <lablancas@gmail.com>
2016-12-09 10:37:56 -0600

    added a gulp task to pull down latest web flow html and "angularize" it

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const prettify = require('gulp-html-prettify');
+const download = require('gulp-downloader');
+const del = require('del');
+const replace = require('gulp-replace');
 
 gulp.task('default', function() {
-  // place code for your default task here
+  del(['index.html']).then(paths => {
+      console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
+
+  download('http://tp-v1.webflow.io/index.html')
+    .pipe(replace(/<\/head>/, 
+      '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.0/angular.min.js"></script><script src="./app.js"></script><script src="./node_modules/angular-uuid/angular-uuid.js"></script></head>'))
+    .pipe(replace(/<body>/, 
+      '<body ng-app="angular-talent-demo" ng-controller="TalentFormController" >'))
+    .pipe(replace(/<form data-name="Email Form"/, '<form ng-submit="submit()"'))
+    .pipe(replace(/(Talent ID): [0-9]+/, '$1: {{talent.id}}'))
+    .pipe(replace(/data-w-tab="Tab ([2-9]|1[0-9])"/g, 'data-w-tab="Tab $1" ng-class="{ \'element-disabled\': !talent.id }"'))
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('./'))
 });
